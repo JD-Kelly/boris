c2b86abed3d2ffedecbcce760bb743e00e6ef8f8
“Jonny <jonny.kelly192@gmail.com>
2021-04-29 14:59:25 +0100

    challenge 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,7 +2,10 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike_docked
   def release_bike
-    Bike.new
+    if @bike_docked == nil 
+      fail "No bikes available"
+    end
+    @bike_docked
   end
 
   def docked(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,18 @@
 require 'docking_station'
     
 describe DockingStation do 
-  it { is_expected.to respond_to :release_bike }
   
+  describe "#release_bike" do
+    it { is_expected.to respond_to :release_bike }
+
+    it "Errors when @bike_docked is empty" do
+      expect {subject.release_bike}.to raise_error "No bikes available"
+    end
+
+  end
+
   it 'releases working bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(bike).to be_working
   end 
 
