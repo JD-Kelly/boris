2b2e8559bb78e739d14d18ceee2a11ce39cc06c5
“Jonny <jonny.kelly192@gmail.com>
2021-04-29 14:40:39 +0100

    challenge 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,11 @@
 require_relative 'bike'
 class DockingStation
+  attr_reader :bike_docked
   def release_bike
     Bike.new
+  end
+
+  def docked(bike)
+    @bike_docked = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,12 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end 
+
+  it { is_expected.to respond_to(:docked).with(1).argument }
+
+  it 'returns docked bike' do 
+    bike = Bike.new
+    subject.docked(bike)
+    expect(subject.bike_docked).to eq bike 
+  end
 end
